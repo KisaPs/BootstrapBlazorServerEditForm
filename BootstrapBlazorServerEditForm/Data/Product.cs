229426e3f5b38c05b4862f905c12dd61dbcb6e12
Kisa PS <lerroy-xbbc@yandex.ru>
2024-11-25 13:58:34 +0300

    Добавьте файлы проекта.

---
+++ BootstrapBlazorServerEditForm/Data/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BootstrapBlazorServerEditForm.Data;
+
+public class Product
+{
+    public static int Id = 1;
+    public string ProductId { get; set; } = Id.ToString();
+    public string ProductName { get; set; } = "Product_" + Id++;
+    public int Price { get; set; } = new Random().Next(10, 10000);
+    public bool IsSale { get; set; }
+    public int PriceSale {  get; set; }
+}
