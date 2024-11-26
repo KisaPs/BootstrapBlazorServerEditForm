6eff86cd37dbec6a715cf5fd789195eed8fd276c
Kisa PS <lerroy-xbbc@yandex.ru>
2024-11-26 11:02:00 +0300

    Commit

--- BootstrapBlazorServerEditForm/Data/Product.cs
+++ BootstrapBlazorServerEditForm/Data/Product.cs
@@ -8,4 +8,17 @@ public class Product
     public int Price { get; set; } = new Random().Next(10, 10000);
     public bool IsSale { get; set; }
     public int PriceSale {  get; set; }
+
+
+    public SaleType SaleType { get; set; }
+    public string TypeDescription1 { get; set; } = "Description Type 1";
+    public string TypeDescription2 { get; set; } = "Description Type 2";
+    public string TypeDescription3 { get; set; } = "Description Type 3";
+}
+
+public enum SaleType
+{
+    Type1,
+    Type2,
+    Type3
 }
